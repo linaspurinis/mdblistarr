1d88d961bc2bc22976e05c4608fc25cacb19c4ef
Linas Purinis <linas.purinis@gmail.com>
2023-03-11 18:58:20 +0200

    Version Number

---
+++ mdblistarr/mdblistrr/templatetags/version.py
@@ -0,0 +1,9 @@
+from django import template
+import time
+import os
+
+register = template.Library()
+
+@register.simple_tag
+def version_date():
+    return f"0.{time.strftime('%m%d', time.gmtime(os.path.getmtime('../')))}"
